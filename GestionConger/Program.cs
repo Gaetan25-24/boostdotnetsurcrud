c5e26fb71a145bb7f5cda0723fec3184db23cdef
Gaetan24-25 <gaetannorbert9@gmail.com>
2025-06-14 14:59:43 +0300

    commit

--- GestionConger/Program.cs
+++ GestionConger/Program.cs
@@ -16,7 +16,7 @@ namespace GestionConger
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormulairePanel.FormConge());
+            Application.Run(new FormulaireMain.FormMain());
         }
     }
 }
